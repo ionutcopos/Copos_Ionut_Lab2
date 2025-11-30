afe649afa9d6c8c025eab1292cbfbc1dc639727b
Ionut Copos <60569655+ionutcopos@users.noreply.github.com>
2025-11-30 23:51:08 +0200

    Add files via upload

---
+++ Models/Author.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Copos_Ionut_Lab2.Models
+{
+	public class Author
+    {
+		public int ID { get; set; }
+		[Display(Name = "First Name")]
+		public string FirstName { get; set; }
+		[Display(Name = "Last Name")]
+		public string LastName { get; set; }
+
+		[Display(Name = "Author Name")]
+		public string FullName => $"{FirstName} {LastName}";
+
+		public ICollection<Book>? Books { get; set; }
+
+	}
+}
